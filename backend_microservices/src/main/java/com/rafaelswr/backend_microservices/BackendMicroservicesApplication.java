7a2dbe9c9dd7fb38114e91f3353e6a1be00903eb
rafaelswr <rafaels.borges91@gmail.com>
2024-01-31 12:08:20 +0000

    Set up app on IntelliJ & initial commit
    Set up App on IntelliJ & initial commit

---
+++ backend_microservices/src/main/java/com/rafaelswr/backend_microservices/BackendMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelswr.backend_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMicroservicesApplication.class, args);
+	}
+
+}
---
+++ backend_microservices/src/test/java/com/rafaelswr/backend_microservices/BackendMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelswr.backend_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
